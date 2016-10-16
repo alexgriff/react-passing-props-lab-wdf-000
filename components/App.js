6924a75f08c2f0a4014bf982e4f2bd4be32ffd60
alexgriff <alexalexgriffith@gmail.com>
2016-10-16 15:47:47 -0400

    pass tests, need to get app to work

--- components/App.js
+++ components/App.js
@@ -2,10 +2,22 @@ const React = require('react');
 
 const FruitBasket = require('./FruitBasket');
 
-const App = () => {
-  return (
-    <FruitBasket />
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state= {
+      fruit: [],
+      filters: [],
+      currentFilter: null
+    }
+  }
+  
+  render () {
+    return (
+      <FruitBasket fruits={ this.state.fruit } />
+    )
+  }
 }
 
 module.exports = App;
--- components/FilteredFruitList.js
+++ components/FilteredFruitList.js
@@ -1,32 +1,54 @@
 const React = require('react');
 const { Component } = require('react');
 
-class FilteredFruitList extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      items: []
-    };
-  }
-
-  componentWillMount() {
-    fetch('/api/fruit')
-      .then(res => res.json())
-      .then(fruit => this.setState({ items: fruit}));
-  }
-
-  render() {
-    const list = !this.props.filter ? this.state.items :
-      this.state.items.filter(i => i.fruit_type == this.props.filter);
-
-    return (
-      <ul className="fruit-list">
-        {list.map((i,idx) => <li key={idx}>{i.char}</li>)}
-      </ul>
-    );
-  }
+const FilteredFruitList = (props) => {
+  const fruitList = 
+    props.filter
+    ? props.fruit.filter( froot => froot.fruit_type === props.filter )
+    : props.fruit
+  
+  return (
+    <ul className="fruit-list">
+      {
+        fruitList.map( (froot, i) =>
+          <li key={i}>{ froot.char }</li>
+        )
+      }
+    </ul>
+  )
 }
+
+FilteredFruitList.defaultProps = {
+  fruit: [],
+  filter: null
+}
+
+
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+//       items: []
+//     };
+//   }
+
+//   componentWillMount() {
+//     fetch('/api/fruit')
+//       .then(res => res.json())
+//       .then(fruit => this.setState({ items: fruit}));
+//   }
+
+//   render() {
+//     const list = !this.props.filter ? this.state.items :
+//       this.state.items.filter(i => i.fruit_type == this.props.filter);
+
+//     return (
+//       <ul className="fruit-list">
+//         {list.map((i,idx) => <li key={idx}>{i.char}</li>)}
+//       </ul>
+//     );
+//   }
+// }
 
 module.exports = FilteredFruitList;
 
